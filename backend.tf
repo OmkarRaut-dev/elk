a5789ba50d64b4b8e624e05a0b98dc77fb5c2180
Omkar R <omkaraut07@gmail.com>
2022-08-16 18:20:43 +0530

    provider changed

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
-  backend "s3" {
-    bucket         = "talacademy-so1-tflab-bucket"
-    key            = "talent-academy/elk_infra/terraform.tfstate"
-    region         = "eu-west-1"
+  backend "s3" { 
+    bucket = "talent-academy-tf-by-omkar"
+    key = "Talent-Academy/Talent-Academy/Terraform_Labs/elk/terraform.tfstate"
+    region = "eu-west-1"
     dynamodb_table = "terraform-lock"
   }
 }
